28ba7c1c9b6ccd3a48e580e6992ba13004e4a450
MayraCuellar <mayrabeyota@hotmail.com>
2023-08-29 03:50:49 +0000

    table created

--- js/app.js
+++ js/app.js
@@ -10,13 +10,64 @@ let hours = ['6am', '7am', '8am','9am','10am','11am','12pm','1pm','2pm','3pm','4
 
 const storesArray = [];
 
-// *** Helper Functions or Utilities *** //
+let salesTable;
+
+// **** Helper Functions or Utilities **** //
+
+// **creating table
+
+function renderTable(){
+  let table = document.createElement('table');
+  locationSection.appendChild(table);
+  // Apply table element to global variable salesTable.
+  // This will allow that all table elements from render() can attach to this one table
+  salesTable = table;
+}
+
+
+function renderHeader(){
+
+  // Create row that will display hours
+  let rowHeading = document.createElement('tr');
+  salesTable.appendChild(rowHeading);
+
+  // Create empty firs cell
+  let cell = document.createElement('th');
+  rowHeading.appendChild(cell);
+
+  // Create for loop to display hours across rowHeading
+  for (let i = 0; i < hours.length; i++){
+    let hourHeading = document.createElement('th');
+    hourHeading.textContent = hours[i];
+    rowHeading.appendChild(hourHeading);
+  }
+  // Create last cell displaying text 'Daily Location Total'
+  let totalCookiesSoldHeader = document.createElement('th');
+  totalCookiesSoldHeader.textContent = 'Daily Location Total';
+  rowHeading.appendChild(totalCookiesSoldHeader);
+
+}
+
+function renderFooter(){
+  // Create row that will display hours
+  let rowFooter = document.createElement('tr');
+  salesTable.appendChild(rowFooter);
+
+  //Create last cell displaying text 'Totals'
+  let totalsFooter = document.createElement('th');
+  totalsFooter.textContent = 'Totals';
+  rowFooter.appendChild(totalsFooter);
+
+}
+
 
 function renderAll(){
   for (let i = 0; i < storesArray.length; i++){
     storesArray[i].render();
   }
+
 }
+
 
 // **** CONSTRUCTOR FUNCTION ****
 
@@ -62,37 +113,26 @@ Stores.prototype.render = function(){
   this.cookiesPurchasedHourly = this.getCookiesPurchasedHourly(); // this method call will populate the amount of cookies purchased hourly in Seattle
   this.totalCookiesSold = this.getTotalCookiesSold(); // this method call will populate the amount of total cookies sold per day in Seattle
 
-  // *** Creating element: section
-  let sectionElement = document.createElement('article');
+  //Create row to display each store name and daily sales data
+  let row = document.createElement('tr');
+  salesTable.appendChild(row);
 
-  // *** Add to DOM*** parent.appendChild(child)
+  // Create table header for name of store
+  let th1Elem = document.createElement('th');
+  th1Elem.textContent = this.storeName;
+  row.appendChild(th1Elem);
 
-  locationSection.appendChild(sectionElement);
-
-
-  let locationHeading = document.createElement('h2');
-  locationHeading.innerText = this.storeName;
-  sectionElement.appendChild(locationHeading);
-
-  let locationUL = document.createElement('ul');
-  sectionElement.appendChild(locationUL);
-
-  for (let i = 0; i < hours.length; i++){
-    let hoursLI = document.createElement('li');
-    hoursLI.innerText = `${hours[i]}: ${this.cookiesPurchasedHourly[i]} cookies`;
-    locationUL.appendChild(hoursLI);
+  // Create table data to display daily sales of cookies
+  for(let i = 0; i < this.cookiesPurchasedHourly.length; i++) {
+    let tdElem = document.createElement('th');
+    tdElem.textContent = this.cookiesPurchasedHourly[i];
+    row.appendChild(tdElem);
   }
-
-  let cookieTotal = document.createElement('li');
-  cookieTotal.innerText = `Total Sales: ${this.totalCookiesSold} cookies`;
-  locationUL.appendChild(cookieTotal);
-
+  // Create table data to display daily location totals
+  let totalsElem = document.createElement('th');
+  totalsElem.textContent = this.totalCookiesSold;
+  row.appendChild(totalsElem);
 };
-
-
-
-
-
 
 // ****Executable code****
 
@@ -108,11 +148,14 @@ let Lima = new Stores('Lima', 2, 16, 4.6);
 
 storesArray.push(Seattle, Tokyo, Dubai, Paris, Lima);
 
+renderTable();
+renderHeader();
 renderAll();
+renderFooter();
 
-console.log(Seattle, Tokyo, Dubai, Paris, Lima);
+//console.log(Seattle, Tokyo, Dubai, Paris, Lima);
+
 /*
-
 
 
 
